899059629566bcc1561b0c7751438139632a0e3d
Killface1980 <tmgaertner@googlemail.com>
2016-03-01 21:47:04 +0100

    Added files via upload

---
+++ Source/RoadsUnited_Americana.cs
@@ -0,0 +1,13 @@
+using ICities;
+
+namespace MyMod {
+    public class MyModInfo : IUserMod {
+        public string Name {
+            get { return "Roads United Americana"; }
+        }
+
+        public string Description {
+            get { return "Roads United Theme Americana"; }
+        }
+    }
+}
