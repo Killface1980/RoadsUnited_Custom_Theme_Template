0f43982289f6c5890c558bc65f542e29efa20107
Killface1980 <tmgaertner@googlemail.com>
2016-03-02 23:31:57 +0100

    Update RoadsUnited_Americana.cs

--- Source/RoadsUnited_Americana.cs
+++ Source/RoadsUnited_Americana.cs
@@ -3,11 +3,11 @@ using ICities;
 namespace MyMod {
     public class MyModInfo : IUserMod {
         public string Name {
-            get { return "Roads United Americana"; }
+            get { return "Roads United: Americana"; }
         }
 
         public string Description {
-            get { return "Roads United Theme Americana"; }
+            get { return "Roads United Theme American Style from American Roads. Not for public acccess."; }
         }
     }
 }
